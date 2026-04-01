59f7adf994f63add1aa58ea6cf454395729a697f
dakiauz <muhammad.u@dakia.ai>
2026-04-01 16:19:40 +0500

    Built the Chat widget_test_local

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -14,6 +15,7 @@ export default defineConfig(({ mode }) => {
         },
         plugins: [
             react(),
+            cssInjectedByJsPlugin(),
         ],
         resolve: {
             alias: {
