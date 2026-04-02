1c676f9d394f495603f470ae0b28de9b92833296
dakiauz <muhammad.u@dakia.ai>
2026-04-02 20:25:05 +0500

    MAtching the envs

--- vite.config.ts
+++ vite.config.ts
@@ -48,6 +48,9 @@ export default defineConfig(({ mode }) => {
             outDir: 'dist-widget',
         } : {
             outDir: 'dist',
+            commonjsOptions: {
+                transformMixedEsModules: true,
+            },
         },
     };
 });
